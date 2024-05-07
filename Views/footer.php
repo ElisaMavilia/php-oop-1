99966d264e56483eaec3c3109f6d0fca26d9ee15
ElisaMavilia <elisamavilia1@gmail.com>
2024-05-07 15:20:27 +0200

    init

---
+++ Views/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p class="text-center">Made with &hearts; by Elisa</p>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+include __DIR__ . '/Views/header.php';
+
+?>
+<main class="container">
+    ciao
+</main>
+<?php include __DIR__ .'/Views/footer.php'; ?>
